cb3cac7991112afe4209418bb759767f5dec16ac
Anastasiia Yarmoliuk <anastasiaarmoluk18@gmail.com>
2026-05-04 21:34:15 +0300

    fix: change webServer command to npm run dev

--- playwright.config.js
+++ playwright.config.js
@@ -76,8 +76,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
 });
